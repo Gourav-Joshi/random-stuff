00b5dfd04df8c8e14ca302170631d26538155761
Gourav Joshi <gourav.joshi@nokia.com>
2024-05-07 22:35:12 +0530

    Chatgpt ka upyog krte hue

--- photos_sort.py
+++ photos_sort.py
@@ -11,9 +11,6 @@ default values :
 
 Don't trust me : Always check all options before executing.
 '''
-
-
-
 import argparse
 import os
 import shutil
@@ -21,13 +18,13 @@ import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script for me as a photographer to keep same copy of jpegs/jpgs & raw files in separate folder")
-    parser.add_argument('-r', '--raw_folder', help='Raw folder path')
-    parser.add_argument('-j', '--jpeg_folder', help='JPEG folder path')
-    parser.add_argument('-re', '--raw_extension', default='CR3', help='Raw file extension')
-    parser.add_argument('-e', '--jpeg_extension', default='jpeg', help='JPEG file extension')
-    parser.add_argument('-d', '--delete', action='store_true', help='Delete = True else just prints messages')
+    parser.add_argument('-r', '--raw_folder', help='Path to the RAW folder')
+    parser.add_argument('-j', '--jpeg_folder', help='Path to the JPEG folder')
+    parser.add_argument('-re', '--raw_extension', default='CR3', help='RAW file extension (default: CR3)')
+    parser.add_argument('-e', '--jpeg_extension', default='jpeg', help='JPEG file extension (default: jpeg)')
+    parser.add_argument('-d', '--delete', action='store_true', help='Delete RAW files not present in JPEG folder (default: False)')
     args = parser.parse_args()
-    print(args)
+
     raw_folder = args.raw_folder
     jpeg_folder = args.jpeg_folder
     raw_extension = args.raw_extension
@@ -35,22 +32,18 @@ if __name__ == '__main__':
     is_delete = args.delete
     delete_dir = 'deleted_raw'
 
-    if not os.path.exists(os.path.join(raw_folder, delete_dir)):
-        print(os.path.join(raw_folder, delete_dir))
-        if is_delete:
-            os.makedirs(os.path.join(raw_folder, delete_dir))            
-
-    jpeg_pics = [ i.split('.{}'.format(jpeg_extension))[0] for i in os.listdir(jpeg_folder) if os.path.isfile(os.path.join(jpeg_folder, i))]
-    raw_pics = [ i.split('.{}'.format(raw_extension))[0] for i in os.listdir(raw_folder) if os.path.isfile(os.path.join(raw_folder, i))]
-
-    if len(jpeg_pics) != len(set(jpeg_pics)) or len(raw_pics) != len(set(raw_pics)) :
-        sys.exit("Exit because same file name with different extensions exists")
+    if is_delete and not os.path.exists(os.path.join(raw_folder, delete_dir)):
+        print("RAW files to delete are under this directory", os.path.join(raw_folder, delete_dir))
+        os.makedirs(os.path.join(raw_folder, delete_dir))            
     
-    jpeg_pics = set(jpeg_pics)
-    raw_pics = set(raw_pics)
+    jpeg_files = set(os.path.splitext(file)[0] for file in os.listdir(jpeg_folder) if os.path.isfile(os.path.join(jpeg_folder, file)))
+    raw_files = set(os.path.splitext(file)[0] for file in os.listdir(raw_folder) if os.path.isfile(os.path.join(raw_folder, file)))
 
-    pics_to_delete_from_raw = raw_pics.difference(jpeg_pics)
+    if len(jpeg_files) != len([file for file in os.listdir(raw_folder) if os.path.isfile(os.path.join(raw_folder, file))]) or len(raw_files) != len([file for file in os.listdir(raw_folder) if os.path.isfile(os.path.join(raw_folder, file))]) :
+        sys.exit("Exit because same file name with different extensions exists")
+
+    pics_to_delete_from_raw = raw_files.difference(jpeg_files)
     for file in pics_to_delete_from_raw:
-        print("Item to delete : {}".format(os.path.join(raw_folder, "{}.{}".format(file, raw_extension))))
+        print("Item to delete : {}.{}".format(file, raw_extension))
         if is_delete:
             shutil.move(os.path.join(raw_folder, "{}.{}".format(file, raw_extension)), os.path.join(raw_folder, delete_dir))
